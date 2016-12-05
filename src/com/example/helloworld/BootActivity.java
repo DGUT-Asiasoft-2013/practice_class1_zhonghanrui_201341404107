d60c3cb1d8f7c796fa1513d7c1d5d4cf0880fcd2
Administrator <Administrator@7A304-0041>
2016-12-05 17:19:07 +0800

    delete empty line

--- src/com/example/helloworld/BootActivity.java
+++ src/com/example/helloworld/BootActivity.java
@@ -14,8 +14,6 @@ public class BootActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_boot);
-		
-		
 	}
 	
 	@Override
