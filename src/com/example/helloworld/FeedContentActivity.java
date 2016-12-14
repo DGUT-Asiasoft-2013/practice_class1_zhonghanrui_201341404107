661456851a00e0d7ddefa8be77ba3e49fa6b9c0a
Administrator <Administrator@7A304-0041>
2016-12-14 10:49:02 +0800

    发表评论

--- src/com/example/helloworld/FeedContentActivity.java
+++ src/com/example/helloworld/FeedContentActivity.java
@@ -1,14 +1,20 @@
 package com.example.helloworld;
 
+import java.text.SimpleDateFormat;
+
 import com.example.helloworld.entity.Article;
 import com.example.helloworld.view.AvatarView;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.format.DateFormat;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class FeedContentActivity extends Activity {
+public class FeedContentActivity extends Activity implements OnClickListener {
 
 	private Article article;
 	private TextView tvTitle;
@@ -16,6 +22,7 @@ public class FeedContentActivity extends Activity {
 	private TextView tvTime;
 	private AvatarView avatarView;
 	private TextView tvName;
+	private Button btnComment;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,10 +33,26 @@ public class FeedContentActivity extends Activity {
 		tvTime=(TextView) findViewById(R.id.tvTime);
 		tvName=(TextView) findViewById(R.id.tvName);
 		avatarView=(AvatarView) findViewById(R.id.avatar);
+		btnComment=(Button) findViewById(R.id.btnComment);
 		tvTitle.setText(article.getTitle());
 		tvContent.setText(article.getText());
-		tvTime.setText(new DateFormat().format("yyyy-MM-dd HH:mm", article.getCreateDate()));
+		tvTime.setText(new SimpleDateFormat("yyyy-MM-dd HH:mm").format(article.getCreateDate()));
 		tvName.setText(article.getAuthorName());
-		avatarView.load(article.getAuthorAvatar());
+		avatarView.load(article.getAuthorAvatar()==null?"upload/face.jpg":article.getAuthorAvatar());
+		tvContent.setMovementMethod(ScrollingMovementMethod.getInstance());
+		btnComment.setOnClickListener(this);
+	}
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btnComment:
+			Intent intent=new Intent(this,AddCommentActivity.class);
+			intent.putExtra("article", article);
+			startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
 	}
 }
